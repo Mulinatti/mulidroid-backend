1b1a3ad3f39c81712f0ab80ce36488a3b843774c
mulinatti <gabrielmulinari2002@gmail.com>
2024-10-08 18:21:58 -0300

    feat: get service by id route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,12 @@
 import fastify from "fastify";
-import { getServicesRoute } from "./routes/get-services";
-import { getEmployeesRoute } from "./routes/get-employees";
-import { getVehiclesRoute } from "./routes/get-vehicles";
+import { getServicesRoute } from "./routes/get-services-route";
+import { getEmployeesRoute } from "./routes/get-employees-route";
+import { getVehiclesRoute } from "./routes/get-vehicles-route";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { createEmployeesRoute } from "./routes/create-employee";
-import { createServiceRoute } from "./routes/create-service";
-import { createVehicleRoute } from "./routes/create-vehicle";
+import { createEmployeesRoute } from "./routes/create-employee-route";
+import { createServiceRoute } from "./routes/create-service-route";
+import { createVehicleRoute } from "./routes/create-vehicle-route";
+import { getServiceByIdRoute } from "./routes/get-service-by-id-route";
 
 const app = fastify();
 
@@ -15,6 +16,9 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(getEmployeesRoute);
 app.register(getServicesRoute);
 app.register(getVehiclesRoute);
+
+app.register(getServiceByIdRoute);
+
 app.register(createEmployeesRoute);
 app.register(createServiceRoute);
 app.register(createVehicleRoute);
