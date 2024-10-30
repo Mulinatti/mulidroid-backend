40d10cb950a52fe604f76fcd25d9e852742cbfa8
mulinatti <gabrielmulinari2002@gmail.com>
2024-10-30 18:46:30 -0300

    fix: database_url

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 const envSchema = z.object({
-  DATABASE_URL: z.string().url().default("postgresql://mulidroid:mdroid@localhost:5432/mulidroid"),
+  DATABASE_URL: z.string().url().default("postgresql://mulidroid:mdroid@pg:5432/mulidroid"),
   PORT: z.string().default("10000")
 });
 
