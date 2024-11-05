1a278c3059357835ab687f2718291a9b546a559e
mulinatti <gabrielmulinari2002@gmail.com>
2024-11-05 03:29:07 -0300

    fix: env.ts

--- src/env.ts
+++ src/env.ts
@@ -3,8 +3,8 @@ import z from "zod"
 const envSchema = z.object({
 	DATABASE_URL: z.string().url().default(JSON.stringify(process.env.DATABASE_URL)),
 	PORT: z.string().default("10000"),
-	SERVICE_EMAIL: z.string(),
-	SERVICE_PASSWORD: z.string(),
+	SERVICE_EMAIL: z.string().default(JSON.stringify(process.env.SERVICE_EMAIL)),
+	SERVICE_PASSWORD: z.string().default(JSON.stringify(process.env.SERVICE_PASSWORD)),
 })
 
 export const env = envSchema.parse(process.env)
