ec6e93e2cb081e78ddfd1c2b04c78fddc0cd381e
mulinatti <gabrielmulinari2002@gmail.com>
2024-11-05 03:06:31 -0300

    fix: params in schema

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,43 +1,51 @@
-import fastify from "fastify"
-import { getServicesRoute } from "./routes/get/get-services-route"
-import { getEmployeeRoute } from "./routes/get/get-employees-route"
-import { getVehiclesRoute } from "./routes/get/get-vehicles-route"
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod"
-import { createEmployeesRoute } from "./routes/post/create-employee-route"
-import { createServiceRoute } from "./routes/post/create-service-route"
-import { createVehicleRoute } from "./routes/post/create-vehicle-route"
-import { getServiceByIdRoute } from "./routes/get/get-service-by-id-route"
-import { getEmployeeByIdRoute } from "./routes/get/get-employee-by-id-route"
-import { env } from "../env"
-import { deleteEmployeeByIdRoute } from "./routes/delete/delete-employee-by-id-route"
-import { deleteServiceByIdRoute } from "./routes/delete/delete-service-by-id-route"
-import { deleteVehicleByIdRoute } from "./routes/delete/delete-vehicle-by-id-route"
+import fastify from "fastify";
+import { getServicesRoute } from "./routes/get/get-services-route";
+import { getEmployeeRoute } from "./routes/get/get-employees-route";
+import { getVehiclesRoute } from "./routes/get/get-vehicles-route";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createEmployeesRoute } from "./routes/post/create-employee-route";
+import { createServiceRoute } from "./routes/post/create-service-route";
+import { createVehicleRoute } from "./routes/post/create-vehicle-route";
+import { getServiceByIdRoute } from "./routes/get/get-service-by-id-route";
+import { getEmployeeByIdRoute } from "./routes/get/get-employee-by-id-route";
+import { env } from "../env";
+import { deleteEmployeeByIdRoute } from "./routes/delete/delete-employee-by-id-route";
+import { deleteServiceByIdRoute } from "./routes/delete/delete-service-by-id-route";
+import { deleteVehicleByIdRoute } from "./routes/delete/delete-vehicle-by-id-route";
+import { updateEmployeesByIdRoute } from "./routes/put/update-employee-by-id-route";
+import { updateServiceByIdRoute } from "./routes/put/update-service-by-id-route";
 
-const app = fastify()
+const app = fastify();
 
-app.setValidatorCompiler(validatorCompiler)
-app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.register(getEmployeeRoute)
-app.register(getServicesRoute)
-app.register(getVehiclesRoute)
+app.register(getEmployeeRoute);
+app.register(getServicesRoute);
+app.register(getVehiclesRoute);
 
-app.register(getServiceByIdRoute)
-app.register(getEmployeeByIdRoute)
+app.register(getServiceByIdRoute);
+app.register(getEmployeeByIdRoute);
 
-app.register(createEmployeesRoute)
-app.register(createServiceRoute)
-app.register(createVehicleRoute)
+app.register(createEmployeesRoute);
+app.register(createServiceRoute);
+app.register(createVehicleRoute);
 
-app.register(deleteEmployeeByIdRoute)
-app.register(deleteServiceByIdRoute)
-app.register(deleteVehicleByIdRoute)
+app.register(deleteEmployeeByIdRoute);
+app.register(deleteServiceByIdRoute);
+app.register(deleteVehicleByIdRoute);
+
+app.register(updateEmployeesByIdRoute);
+app.register(updateServiceByIdRoute);
 
 app
-	.listen({
-		host: "0.0.0.0",
-		port: Number.parseInt(env.PORT),
-	})
-	.then(() => {
-		console.log(`Server Running at PORT ${env.PORT}`)
-	})
+  .listen({
+    host: "0.0.0.0",
+    port: Number.parseInt(env.PORT),
+  })
+  .then(() => {
+    console.log(`Server Running at PORT ${env.PORT}`);
+  });
--- src/server/routes/get/get-employee-by-id-route.ts
+++ src/server/routes/get/get-employee-by-id-route.ts
@@ -1,16 +1,20 @@
-import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
-import type { FastifyRequest } from "fastify"
-import { getEmployeeById } from "../../../services/get/get-employee-by-id"
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { getEmployeeById } from "../../../services/get/get-employee-by-id";
+import z from "zod";
 
-type employeeIdRequest = FastifyRequest<{
-	Params: {
-		employeeId: string
-	}
-}>
-
-export const getEmployeeByIdRoute: FastifyPluginAsyncZod = async app => {
-	app.get("/employee/:employeeId", async ({ params }: employeeIdRequest) => {
-		const employee = await getEmployeeById(params.employeeId)
-		return employee
-	})
-}
+export const getEmployeeByIdRoute: FastifyPluginAsyncZod = async (app) => {
+  app.get(
+    "/employee/:employeeId",
+    {
+      schema: {
+        params: z.object({
+          employeeId: z.string(),
+        }),
+      },
+    },
+    async ({ params }) => {
+      const employee = await getEmployeeById(params.employeeId);
+      return employee;
+    }
+  );
+};
