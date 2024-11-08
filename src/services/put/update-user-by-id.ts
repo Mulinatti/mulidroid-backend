16cfd538c75ff5262232baa9b0a63d4bdd945024
Gabriel de Paulo Mulinari <79599663+Mulinatti@users.noreply.github.com>
2024-11-08 07:17:14 -0300

    Update update-user-by-id.ts

--- src/services/put/update-user-by-id.ts
+++ src/services/put/update-user-by-id.ts
@@ -1,9 +1,11 @@
 import { eq } from "drizzle-orm"
 import { db } from "../../db"
 import { user } from "../../db/schema"
-import type { IUserPost } from "../../interfaces/IUser"
+import { hash } from "bcrypt"
 
 export const updateUserById = async ({ username, password }: IUserPost, employeeId: string) => {
+	const newPassword = await hash(password, 5);
+	
 	await db
 		.update(user)
 		.set({
